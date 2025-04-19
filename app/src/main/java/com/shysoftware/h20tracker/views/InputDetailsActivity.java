f504261758eee7b7d4de62125db154d678c023c7
Nash Adam Munoz <150409949+lichclass@users.noreply.github.com>
2025-04-19 23:37:21 +0800

    chore: Refactoring for Input details

--- app/src/main/java/com/shysoftware/h20tracker/views/InputDetailsActivity.java
+++ app/src/main/java/com/shysoftware/h20tracker/views/InputDetailsActivity.java
@@ -2,15 +2,11 @@ package com.shysoftware.h20tracker.views;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.shysoftware.h20tracker.R;
 
-public class InputDetails extends AppCompatActivity {
+public class InputDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
